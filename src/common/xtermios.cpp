77065cba36f3db66e53fe37ed5ba7f59ea59dd19
dmitry <danchenko.dmitry@gmail.com>
2025-12-30 16:25:12 +0200

    Fix GCC 15.2.0: use memset + individual assignments instead of array initializer
    GCC 15.2.0 generates invalid .base64 assembler pseudo-op when using
    array initializers with multiple values. Work around by using memset
    to zero the array and then assigning individual elements.

--- src/common/xtermios.cpp
+++ src/common/xtermios.cpp
@@ -25,12 +25,22 @@ int reset_uart(int fd)
 {
     struct termios tc = {};
     /* See termios(3) */
-    /* Workaround for GCC 15.2.0 bug: use decimal instead of octal to avoid .base64 assembler error */
-    const cc_t default_cc[]
-        = {3, 28, 127, 21, 4, 0, 0, 0, 17, 19, 26, 0, 18, 15, 23, 22,
-           0, 0,  0,   0,  0, 0, 0, 0, 0,  0,  0,  0, 0,  0,  0,  0};
-
-    /* Workaround for GCC 15.2.0: use runtime check instead of static_assert */
+    /* Workaround for GCC 15.2.0 bug: use memset + individual assignments to avoid .base64 assembler error */
+    cc_t default_cc[32];
+    memset(default_cc, 0, sizeof(default_cc));
+    default_cc[0] = 3;    /* VINTR: Ctrl-C */
+    default_cc[1] = 28;   /* VQUIT: Ctrl-\ */
+    default_cc[2] = 127; /* VERASE: DEL */
+    default_cc[3] = 21;   /* VKILL: Ctrl-U */
+    default_cc[4] = 4;    /* VEOF: Ctrl-D */
+    default_cc[8] = 17;  /* VSTART: Ctrl-Q */
+    default_cc[9] = 19;  /* VSTOP: Ctrl-S */
+    default_cc[10] = 26; /* VSUSP: Ctrl-Z */
+    default_cc[11] = 18; /* VREPRINT: Ctrl-R */
+    default_cc[12] = 15; /* VDISCARD: Ctrl-O */
+    default_cc[13] = 23; /* VWERASE: Ctrl-W */
+    default_cc[14] = 22; /* VLNEXT: Ctrl-V */
+    
     if (sizeof(default_cc) != sizeof(tc.c_cc)) {
         return -1; /* Unknown termios struct with different size */
     }
